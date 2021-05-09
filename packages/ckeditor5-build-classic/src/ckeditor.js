41262174f746ba03f687a04e99d92f694858647a
jtomambid <shenztomambid@gmail.com>
2021-05-10 02:16:12 +0800

    custom build - add font, special characters and ckfinder

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -30,7 +30,7 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
-
+import Font from '@ckeditor/ckeditor5-font/src/font';
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -58,7 +58,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Font
 ];
 
 // Editor configuration.
@@ -81,7 +82,10 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'ckfinder',
+			'fontFamily',
+			'fontSize'
 		]
 	},
 	image: {
@@ -91,6 +95,9 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'imageTextAlternative'
 		]
+	},
+	ckfinder: {
+
 	},
 	table: {
 		contentToolbar: [
